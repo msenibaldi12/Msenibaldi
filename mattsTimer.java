a3f92c808728f4e13ced372dae391ff9e65a00ac
msenibaldi12 <51300613+msenibaldi12@users.noreply.github.com>
2020-09-06 20:32:58 -0400

    Added notes and the header

--- mattsTimer.java
+++ mattsTimer.java
@@ -1,21 +1,25 @@
 package msenibaldi;
-
+/* Author: Matt Senibaldi
+ * 9/6/2020
+ * Class1
+ * This program is a basic timer
+ */
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class mattsTimer {
 
-	
+	// Setting the start time to 0 seconds 
 		int secondsPassed = 0;
 		
 		Timer myTimer = new Timer();
 		TimerTask task = new TimerTask() {
 			public void run() {
-				secondsPassed++;
+				secondsPassed++;  // Adding 1 second 
 				System.out.println("Time Passed: " + secondsPassed);
 			}
 		};
-		public void start() {
+		public void start() {    // Settings the interval for the timer, 1 second
 			myTimer.scheduleAtFixedRate(task, 1000, 1000);
 		}
 		public static void main(String[] args) {
